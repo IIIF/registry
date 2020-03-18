82e2d15fb9db1445051f14ec25b9d7d124941b34
Glen Robson <glen.robson@gmail.com>
2020-03-18 17:39:07 +0000

    Adding code to find all files that are json

--- src/createAS.py
+++ src/createAS.py
@@ -4,19 +4,28 @@ import json
 
 from os import listdir
 from os.path import isfile, join, isdir
+import re
 
+ignore_list = ['[.]+.*', 'src']
 
 def processDir(path):
     for fileName in listdir(path):
-        print (fileName[-5:])
-        if isdir(fileName):
-            processDir(fileName)
-        elif fileName[-5:] == '.json':    
-            # process JSON
-            with open('{}/{}'.format(path, fileName)) as json_file:
-                data = json.load(json_file)
-        else:
-            print ('Ignoring {}'.format(fileName))
+        ignore=False
+        for ignoreStr in ignore_list:
+            if re.match(ignoreStr, fileName):
+                #print ('Matched {} with {}'.format(fileName, ignoreStr))
+                ignore=True
+                break
+        if not ignore:
+            if isdir(fileName):
+                processDir(fileName)
+            elif fileName[-5:] == '.json':    
+                print ('Processing: {}'.format(fileName))
+                # process JSON
+                with open('{}/{}'.format(path, fileName)) as json_file:
+                    data = json.load(json_file)
+            #else:
+            #    print ('Ignoring {} as its not json'.format(fileName))
             
     
 
